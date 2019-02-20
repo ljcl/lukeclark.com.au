ecd9fd2225ab24c4f5b5da8598793cfa71e73365
Luke Clark <luke@lukeclark.com.au>
2019-02-20 20:54:19 +1100

    Minor bio tweaks

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -5,7 +5,7 @@ import profileImg from './profile-img.jpg';
 
 const bioStyle = css`
   display: flex;
-  margin-bottom: ${rhythm(2)};
+  margin-bottom: ${rhythm(1)};
 `;
 
 const bioTextStyle = css`
@@ -15,16 +15,29 @@ const bioTextStyle = css`
 `;
 
 const bioImgStyle = css`
-  margin-right: ${rhythm(1 / 3)};
+  border: 3px solid #010406;
+  border-radius: 100%;  
+  margin-right: ${rhythm(1 / 2)};
   margin-bottom: 0;
-  width: ${rhythm(2)};
-  height: ${rhythm(2)};
-  border-radius: 100%;
+  width: ${rhythm(2.2)};
+  min-width: ${rhythm(2.2)};
+  height: ${rhythm(2.2)};
+
+  img {
+    border: 2px solid #fff;
+    border-radius: 100%;
+    width: ${rhythm(2)};
+    height: ${rhythm(2)};
+    margin-bottom: 0;
+    display: block;
+  }
 `;
 
 const Bio = () => (
   <div css={bioStyle}>
-    <img src={profileImg} css={bioImgStyle} alt={`Luke Clark`} />
+    <figure css={bioImgStyle}>
+      <img src={profileImg} alt={`Luke Clark`} />
+    </figure>
     <p css={bioTextStyle}>
       Personal site of{' '}
       <a href="https://mobile.twitter.com/lukejclark">Luke Clark</a>. Links,
