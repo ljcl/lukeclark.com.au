617a0da14a5469db7cf3e06c580ca8182c9fd6a8
Luke Clark <luke@lukeclark.com.au>
2020-01-03 16:53:03 +1100

    Swap to react-intersection-observer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,56 @@
 /** @jsx jsx */
+import * as React from 'react'
 import { css, jsx } from '@emotion/core';
+import { useInView } from 'react-intersection-observer'
 import { rhythm } from '../utils/typography';
-import Observer from './Observer/Observer';
 import LastFmNowPlaying from './LastFmNowPlaying';
 
-const Footer = () => (
-  <footer
-    css={css`
+const Footer = () => {
+  const [ref, inView] = useInView({
+    rootMargin: "0px 0px 512px 0px",
+    threshold: 1,
+    triggerOnce: true
+  })
+  return (
+    <footer
+      css={css`
       margin-top: ${rhythm(3)};
       margin-bottom: ${rhythm(1)};
     `}
-  >
-    <a
-      href="https://instagram.com/ljcl"
-      target="_blank"
-      rel="noopener noreferrer"
     >
-      gram
+      <a
+        href="https://instagram.com/ljcl"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        gram
     </a>
-    {', '}
-    <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
-      rss
+      {', '}
+      <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+        rss
     </a>
-    {', '}
-    <a href="https://noot.space" target="_blank" rel="noopener noreferrer">
-      noot
+      {', '}
+      <a href="https://noot.space" target="_blank" rel="noopener noreferrer">
+        noot
     </a>
-    {', '}
-    <a
-      href="https://mobile.twitter.com/lukejclark"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      tweet
+      {', '}
+      <a
+        href="https://mobile.twitter.com/lukejclark"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        tweet
     </a>
-    <div
-      css={css`
+      <div
+        css={css`
         float: right;
       `}
-    >
-      <Observer
-        rootMargin="0px 0px 512px 0px"
-        threshold={1}
-        triggerOnce
-        observerId="lastFmNowPlaying"
-        render={inView => inView && <LastFmNowPlaying />}
-      />
-    </div>
-  </footer>
-);
+        ref={ref}
+      >
+        {inView && <LastFmNowPlaying />}
+      </div>
+    </footer>
+  )
+};
 
 export default Footer;
